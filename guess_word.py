0fb35487b6e05fe8280a365fcedc738b2ebcc7c0
victoriousnz <38770094+victoriousnz@users.noreply.github.com>
2021-06-10 15:55:56 +1200

    Update guess_word.py

--- guess_word.py
+++ guess_word.py
@@ -1,4 +1,7 @@
 #WORK IN PROGRESS
+#TODO:
+#Figure out how to make 'status' retain previous guesses in the correct position
+
 import random
 
 def get_word():
@@ -10,15 +13,27 @@ def get_word():
 def check(word, guesses):
     """Creates and returns string representation of word
     displaying asterisks for letters not yet guessed."""
-    status = '' # Current status of guess
+    status = "" # Current status of guess
     last_guess = guesses[-1]
     matches = 0 # Number of occurrences of last_guess in word
+    #print("You guessed", last_guess)
 
-    print("You guessed", last_guess)
-    n3 = word.count(last_guess)
-    print("That appears {} times.".format(n3))
-    print("so far you've guessed", guesses)
+    matches = word.count(last_guess)
+      
+    if matches != 0:
+        print("The word has {} {}'s.".format(matches,last_guess))
+        i = 0
+        while i < len(word):
+            if last_guess == word[i]:
+                status += last_guess
+            else:
+                status += "*"
+            i += 1
+    else:
+        print("Sorry, that appears 0 times.")
+        status += "*" * len(word)
 
+    print("You've made {} guesses and gotten {}".format(len(guesses),status))
 
     # Loop through word checking if each letter is in guesses
     #  If it is, append the letter to status
@@ -40,10 +55,10 @@ def main():
     n = len(word) # The number of letters in the random word
     guesses = [] # The list of guesses made so far
     guessed = False
-    print('The word contains {} letters.'.format(n))
+    #print('The word contains {} letters.'.format(n))
 
     while not guessed:
-        guess = input("Guess a {}-letter word: ".format(n))
+        guess = input("Guess a letter or a {}-letter word: ".format(n))
         guess = guess.upper()
         n2 = len(guess)
         if guess == word:
@@ -52,19 +67,14 @@ def main():
         else:
             if n2 == 1:
                 guesses.append(guess)
-                print("Got to check this...")
-                check(word,guesses)
+                print("Checking...")
+                if guesses.count(guess) == 1:
+                    check(word,guesses)
+                else:
+                    print("You already guessed '{}'.".format(guess))
             else:
-                print("nope")
+                print("Invalid entry.")
 
-    # while not guessed:
-    #     guess = input('Guess a letter or a {}-letter word: '.format(n))
-    #     guess = guess.upper()
-    #     if guess == word:
-    #         guessed = True
-        
-
-        
         # Write an if condition to complete this loop.
         # You must set guessed to True if the word is guessed.
         # Things to be looking for:
